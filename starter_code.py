9c7c67acc75b7e471da78d3bbaca64b3cc3e38c2
ranan <C24392253@mytudublin.ie>
2025-12-05 01:39:51 +0000

    adding parsing function below database function

--- starter_code.py
+++ starter_code.py
@@ -13,7 +13,7 @@ import pandas as pd
 # database steup 
 def init_db(conn):
     cursor = conn.cursor()
-    cursor.excute(
+    cursor.execute(
         """
         CREATE TABLE IF NOT EXISTS tunes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -61,6 +61,64 @@ def insert_tunes(conn, tunes):
         tunes
     )
     conn.commit()
+
+# parsing function
+def parse_abc_tune(lines):
+    tune ={
+        "ref_number": None,
+        "title": None,
+        "tune_type": None,
+        "meter": None,
+        "key_sig": None,
+        "raw_abc": "\n".join(lines)
+    }
+
+    for line in lines:
+        if line.startswith("X:"):
+            tune["ref_number"] = line[2:].strip()
+        elif line.startswith("T:"):
+            tune["title"] = line[2:].strip()
+        elif line.startswith("R:"):
+            tune["tune_type"] = line[2:].strip()
+        elif line.startswith("M:"):
+            tune["meter"] = line[2:].strip()
+        elif line.startswith("K:"):
+            tune["key_sig"] = line[2:].strip()
+
+    return tune
+
+
+def parse_abc_file(file_path, book_number):
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+
+    all_tunes = []
+    current = []
+    tune_index = 0
+
+    for line in lines:
+        line_strip = line.rstrip("\n")
+
+        if line_strip.startswith("X:") and current:
+            tune = parse_abc_tune(current)
+            tune["book_number"] = book_number
+            tune["file_name"] = os.path.basename(file_path)
+            tune["tune_index"] = tune_index
+            all_tunes.append(tune)
+            tune_index += 1
+            current = []
+
+        current.append(line_strip)
+
+    if current:
+        tune = parse_abc_tune(current)
+        tune["book_number"] = book_number
+        tune["file_name"] = os.path.basename(file_path)
+        tune["tune_index"] = tune_index
+        all_tunes.append(tune)
+
+    return all_tunes
+    
 # An example of how to create a table, insert data
 # and run a select query
 def do_databasse_stuff():
